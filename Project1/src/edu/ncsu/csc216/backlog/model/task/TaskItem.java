85d6b09106fe04c95f447dbb9b26def7703ce253
dkudo <dkudo@ncsu.edu>
2019-02-22 15:41:03 -0500

    Created Note.java file

--- Project1/src/edu/ncsu/csc216/backlog/model/task/TaskItem.java
+++ Project1/src/edu/ncsu/csc216/backlog/model/task/TaskItem.java
@@ -1,5 +1,6 @@
 package edu.ncsu.csc216.backlog.model.task;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import edu.ncsu.csc216.backlog.model.command.Command;
@@ -25,7 +26,7 @@ public class TaskItem{
 	/** creator of this task */
 	private String creator;
 	/** note of this task*/
-	private Note notes;
+	private ArrayList<Note> notes;
 	/** owner of this task */
 	private String owner;
 	/** whether this task is verified */
@@ -73,7 +74,7 @@ public class TaskItem{
 		this.title = title;
 		this.type = type;
 		this.creator = creator;
-		this.notes = new Note(creator, notes);
+		this.notes.add(new Note(creator, notes));
 		this.taskId = counter;
 		
 		this.state  = new BacklogState();
@@ -188,82 +189,6 @@ public class TaskItem{
 	public String[][] getNotesArray(){
 		return null;
 	}
-	
-	
-	
-	/**
-	 * @author Daiki Kudo
-	 *
-	 */
-	public class Note {
-
-		/** Author of this note*/
-		private String noteAuthor;
-		/** Note about a task */
-		private String noteText;
-
-		/**
-		 * Constructs Note object which contains note and the author's name
-		 * @param noteAuthor : author's name of a task
-		 * @param noteAuthor : note about a task
-		 * @throws IllegalArgumentException if a note or a noteAuthor is invalid
-		 */
-		public Note(String noteAuthor, String noteText) throws IllegalArgumentException{
-			if (noteAuthor == null || noteAuthor.equals("")) {
-				throw new IllegalArgumentException();
-			}
-			this.setNoteAuthor(noteAuthor);
-			
-			if (noteText == null || noteText.equals("")) {
-				throw new IllegalArgumentException();
-			}
-			this.setNoteText(noteText);
-		}
-
-
-
-		/**
-		 * @return the noteAuthor
-		 */
-		public String getNoteAuthor() {
-			return noteAuthor;
-		}
-		
-		/**
-		 * @param noteAuthor the noteAuthor to set
-		 */
-		private void setNoteAuthor(String noteAuthor) {
-			this.noteAuthor = noteAuthor;
-		}
-		
-		/**
-		 * @return the noteText
-		 */
-		public String getNoteText() {
-			return noteText;
-		}
-
-		/**
-		 * @param noteText the noteText to set
-		 */
-		private void setNoteText(String noteText) {
-			this.noteText = noteText;
-		}
-
-		/**
-		 * Note.getNoteArray() returns an array of length 2 with the author at index 0 and the text at index 1. 
-		 * This is used when creating a list of notes to display in the GUI.
-		 * @return noteArray : String array of an author name and text of a note
-		 */
-		public String[] getNoteArray() {
-			String[] noteArray =  {this.getNoteAuthor(), this.getNoteText()};
-			return noteArray;
-		}
-
-
-
-	}
-
 	
 	
 	
