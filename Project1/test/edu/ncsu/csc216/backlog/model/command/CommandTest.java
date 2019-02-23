da479b80ec8a677a1bef12be617be0d8f4a47b53
dkudo <dkudo@ncsu.edu>
2019-02-22 21:50:13 -0500

    Added javadoc

--- Project1/test/edu/ncsu/csc216/backlog/model/command/CommandTest.java
+++ Project1/test/edu/ncsu/csc216/backlog/model/command/CommandTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Tests Command class
+ * @author Daiki Kudo
+ */
 public class CommandTest {
 
 	@Before
